fe90809582e7aa1c7eff332d16a6a2b97c871036
dmitry <d.popov@labtop.digital>
2019-08-07 16:40:36 +0300

    add instagramm

--- src/js/components/Section-instagram.js
+++ src/js/components/Section-instagram.js
@@ -2,25 +2,42 @@ import Swiper from '../imports/import-swiper';
 
 export default class {
   constructor() {
+    this.domSlider = null;
     this.slider = null;
     this.settings = {
       speed: 0,
       allowTouchMove: false,
       loop: true,
       autoplay: {
-        delay: 500,
+        delay: 300,
       },
     };
+    this.mouseOverHandler = null;
+    this.mouseOutHandler = null;
+  }
+
+  play() {
+    this.slider.autoplay.stop();
+  }
+
+  pause() {
+    this.slider.autoplay.start();
   }
 
   init() {
-    this.slider = new Swiper('#instagram-slider', this.settings);
+    this.domSlider = document.getElementById('instagram-slider');
+    this.mouseOverHandler = this.play.bind(this);
+    this.mouseOutHandler = this.pause.bind(this);
+    this.slider = new Swiper(this.domSlider, this.settings);
+    this.domSlider.addEventListener('mouseenter', this.mouseOverHandler);
+    this.domSlider.addEventListener('mouseleave', this.mouseOutHandler);
   }
 
   destroy() {
     if (this.slider) {
       this.slider.destroy();
       this.slider = null;
+      this.domSlider = null;
     }
   }
 
