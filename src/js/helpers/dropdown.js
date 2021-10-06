42abdbfccf75eaeb7a13c4691cf2c2abc0b4741b
xceeder <xceeder@ukr.net>
2021-10-06 16:45:50 +0300

    add comments to dropdown.js

--- src/js/helpers/dropdown.js
+++ src/js/helpers/dropdown.js
@@ -19,16 +19,18 @@ export default ()=>{
         let maxHeight = 0;
         
         selectListEl.forEach((element, index) => {
+            //set max height to open list
             if(index <= (SHOW_ELEMENTS - 1)){
                 maxHeight += element.offsetHeight;
             }
+            //click to list element
             element.addEventListener('click', (event)=>{
                 setTitle(event.target);
                 callbackFunction(event.target, element);
                 hideFilter();
             })
         });
-
+        //set initial styles to select element
         gsap.set(selectList, {
             height: 0,
             overflow: maxHeight>0?'auto': 'hidden'
@@ -40,6 +42,7 @@ export default ()=>{
 
         document.addEventListener("click", toggleDropdown);
 
+        //closes dropdown when click outside
         function toggleDropdown(event) { 
             if (select.contains(event.target)) return;
             hideFilter();
@@ -64,12 +67,13 @@ export default ()=>{
             })
         }
 
+        //set Title from active element
         function setTitle(el){
             currentTitle.innerHTML = el.closest('[data-select-item]').querySelector('span').innerHTML
         }
-
+        
+        //If you need to add some functions after select item, please add here your code
         function callbackFunction(option,element){
-            console.log(element, option)
         }
     }
 }
