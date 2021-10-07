28ba3ecada2e779fe90140c885e55daaf5d87868
Natalia <n.romanchenko@labtop.digital>
2021-10-07 21:00:01 +0300

    update files structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
 import 'regenerator-runtime';
 
 import commonScripts from './common';
-import pageLoader from './page-loader';
 
 document.addEventListener('DOMContentLoaded', () => {
 
   commonScripts(); // fore common scripts
-  pageLoader(); // fire scripts for loaded page
 
 });
