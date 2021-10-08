9be95a4718150d4a44feaa56d79d880141c05e1a
aleksandr volod'ko <a.f1ghter.v@gmail.com>
2021-10-08 04:45:54 +0300

    add stylelint, change indentation settings to tabs (global)

--- gulp/tasks/pages.js
+++ gulp/tasks/pages.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
-// const {series, parallel, task, watch} = require('gulp');
 const config = require('../config.js');
+const template = require('../helpers/pages-template.js');
 
 function pages(cb) {
   let list =  gulp
@@ -9,57 +9,35 @@ function pages(cb) {
   .pipe(require('gulp-modify-file')((content) => {
     let pages = [];
     let pagelist = '';
+    let filesArray = content.split('"');
 
-     const start = `
-     <!doctype html>
-     <html lang="en">
-       <head>
-         <meta charset="utf-8" />
-         <title>pages</title>
-         <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-         <meta name="viewport" content="width=device-width, initial-scale=1" />
-         <link rel="stylesheet" href="/css/app.css" />
-       </head>
-       <body>
-         <main class="pages">
-           <ul class="pages__list">
-     `;
-
-     const end = `
-           </ul>
-         </main>
-       </body>
-     </html>
-     `;
-     let filesArray = content.split('"');
-
-     filesArray.forEach(function(pageFile) {
+    filesArray.forEach(function(pageFile) {
       if (pageFile.includes('.html') && !pageFile.includes('pages.html')) {
         pages.push(pageFile);
       }
-     });
+    });
 
-     pages.sort((a, b)=>{
-       return a.includes('index') ? -1 : 0;
-     });
+    pages.sort((a, b)=>{
+      return a.includes('index') ? -1 : 0;
+    });
 
-     pages.forEach(function(page) {
-       let pagename = '';
+    pages.forEach(function(page) {
+      let pagename = '';
 
-       if (page.includes('index')) {
-         pagename = 'home';
-       } else {
-         pagename = page.replace('.html', '').replace(/-/g, ' ');
-       }
-       
-       pagelist += `
-         <li class="pages__item">
-           <a href="${page}" class="pages__link" target="_blank">${pagename}</a>
-         </li>
-       `;
-     });
-     return `${start}${pagelist}${end}`
-   }))
+      if (page.includes('index')) {
+        pagename = 'home';
+      } else {
+        pagename = page.replace('.html', '').replace(/-/g, ' ');
+      }
+      
+      pagelist += `
+        <li class="pages__item">
+          <a href="${page}" class="pages__link" target="_blank">${pagename}</a>
+        </li>
+      `;
+    });
+    return `${template.start}${pagelist}${template.end}`
+  }))
   .pipe(gulp.dest(config.dest.html));
   return list;
   cb();
