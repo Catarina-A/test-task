57c9b532b84250a9c615b7e1f2b73dcda6f89e2a
NatashaRomanchenko <n.romanchenko@labtop.digital>
2021-12-13 21:45:03 +0200

    update accordeon

--- src/js/helpers/accordeon.js
+++ src/js/helpers/accordeon.js
@@ -3,50 +3,73 @@ import { gsap } from "gsap";
 /**
 * Function for Accordeon - block whose content can be expanded and hidden. For full docs see link.
 * @see https://wiki.bsgdigital.com/ru/developer/accordeon-js
+* @param { Object } [props] - Full list of properties
+* @param { boolean } [props.needCloseOther=true] - Set to true if you want close other accorders when current open
+* @param { number } [props.duration=0.5] - Accordeon animation duration in seconds
+* @param { string } [props.ease="power1.out"] - Name of gsap ease function for accordeon animation
+* @param { number } [props.delay=0] - Accordeon animation delay in seconds
+* @param { callback } [props.onComplete] - onComplete gsap callback
 */
 
-export default () =>{
+export default (props) => {
 
     const accordeonArray = document.querySelectorAll('[data-accordeon-element]');
-    if (accordeonArray.length == 0){ return}
+    if (accordeonArray.length == 0) return;
+
+	if (!props) props = {};
+
+	const needCloseOther = props.needCloseOther || true;
+	const duration = props.duration || 0.5;
+	const ease = props.ease || "power1.out";
+	const delay = props.delay || 0;
+	const onComplete = props.onComplete;
 
     accordeonArray.forEach(element => {
         const button = element.querySelector('[data-accordeon-button]');
         const content = element.querySelector('[data-accordeon-content]');
 
-        gsap.set(content, {
-            height: 0
-        })
+		if (!element.classList.contains('is-open')) {
+			gsap.set(content, {
+	            height: 0
+	        })
+		}
 
-        button.addEventListener('click', ()=>{
-            element.classList.contains('is-open')?close(element, content):open(element, content)
+        button.addEventListener('click', () => {
+            element.classList.contains('is-open') ? close(element, content) : open(element, content)
         })
     })
 
-    function open(element, content){
+    function open(element, content) {
         gsap.to(content, {
             height: 'auto',
-            onStart: ()=>{
+			duration: duration,
+			ease: ease,
+			delay: delay,
+            onStart: () => {
                 element.classList.add('is-open');
-                //closeOther(element);
-                //Turn this on, when you need to close other opened accordeons
-            }
+                if (needCloseOther) closeOther(element);
+            },
+			onComplete: onComplete
         })
     }
 
-    function close(element, content){
+    function close(element, content) {
         gsap.to(content, {
             height: 0,
-            onStart: ()=>{
+			duration: duration,
+			ease: ease,
+			delay: delay,
+            onStart: () => {
                 element.classList.remove('is-open')
-            }
+            },
+			onComplete: onComplete
         })
     }
 
-    function closeOther(current){
+    function closeOther(current) {
         accordeonArray.forEach(element => {
             const content = element.querySelector('[data-accordeon-content]');
-            element != current? close(element, content): false
+            element != current ? close(element, content) : false
         })
     }
 }
