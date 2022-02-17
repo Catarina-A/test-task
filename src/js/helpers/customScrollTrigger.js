f2f8dfce8de64ff9faa29569eb2c1f64acc956a1
NatashaRomanchenko <n.romanchenko@labtop.digital>
2022-02-17 17:29:27 +0200

    fixed customScrollTrigger

--- src/js/helpers/customScrollTrigger.js
+++ src/js/helpers/customScrollTrigger.js
@@ -4,7 +4,7 @@
  * @param { Object } props - Full list of parameters
  * @param { string|Dom7Array|HTMLElement[] } props.elements
  * @param { string|HTMLElement } [props.trigger=props.elements] - Element or Selector of it ancestor that will be trigger for run callback
- * @param { string|HTMLElement } [props.scrollWrapper=document] = Container to track scrolling
+ * @param { string|HTMLElement } [props.scrollWrapper=document.querySelector('[data-scroll-container]')] = Container to track scrolling
  * @param { (number|string) } [props.triggerPoint=document.documentElement.clientHeight] - Number of pixels or Number + '%' for percent of window height is point on window, upon reaching which callback is run
  * @callback callback
  */
@@ -20,12 +20,12 @@ export default (props) => {
 		elems = props.elements;
 	}
 	if (elems.length) {
-		// let scrollWrapper;
-		// if (typeof props.scrollWrapper == 'string') {
-		//   scrollWrapper = document.querySelectorAll(props.scrollWrapper);
-		// } else {
-		//   scrollWrapper = props.scrollWrapper;
-		// }
+		let scrollWrapper;
+		if (typeof props.scrollWrapper == 'string') {
+			scrollWrapper = document.querySelector(props.scrollWrapper);
+		} else {
+			scrollWrapper = props.scrollWrapper;
+		}
 		const wrapper = props.scrollWrapper ? props.scrollWrapper : document;
 		elems.forEach((elem) => {
 			showElem(elem, props.trigger, props.triggerPoint, props.callback);
@@ -47,14 +47,14 @@ function showElem(elem, trigger, triggerPoint, func) {
 	else {
 		container = elem;
 	}
-	const triggerTop = container.getBoundingClientRect().top;
+	const elemTop = container.getBoundingClientRect().top;
 	let triggerPointNum;
-	if (typeof triggerPoint === 'string' && triggerPoint.indexOf('%') !== -1) {
+	if (typeof triggerPoint == 'string' && triggerPoint.indexOf('%') != -1) {
 		const fraction = parseFloat(triggerPoint) / 100;
 		triggerPointNum = document.documentElement.clientHeight * fraction;
 	}
-	else if (typeof parseFloat(triggerPoint) === 'number') {
-		triggerPointNum = triggerPoint;
+	else if (typeof parseFloat(triggerPoint) == 'number') {
+		triggerPointNum = parseFloat(triggerPoint);
 	}
 	else {
 		triggerPointNum = document.documentElement.clientHeight;
