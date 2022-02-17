da25e639b9eeb721c4f966a7851c0a18f1291455
NatashaRomanchenko <n.romanchenko@labtop.digital>
2022-02-17 16:18:19 +0200

    beauty fixes

--- src/js/common.js
+++ src/js/common.js
@@ -2,6 +2,4 @@ import setFullHeight from './helpers/setFullHeight';
 
 export default () => {
 	setFullHeight(); //Set VH variable for mobile safari 100VH, use scss mixin fullheight()
-	console.log('test');
 };
-
